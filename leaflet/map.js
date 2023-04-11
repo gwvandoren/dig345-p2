404255613376c81bf569f4a457fc740437b8ad3f
gwvandoren <gwvandoren@davidson.edu>
2023-04-10 20:24:10 -0400

    started yay

---
+++ leaflet/map.js
@@ -0,0 +1,4 @@
+var map = L.map('map').setView([35.5014,-80.845296], 17);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
