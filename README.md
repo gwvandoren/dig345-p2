9b55aa9d72ec06f29f1e4c359aba79cdc8e6c176
gwvandoren <gwvandoren@davidson.edu>
2023-05-02 21:17:41 -0400

    overdue update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # dig345-p2
+include list of libraries i used
---
+++ readerLocations.js
@@ -0,0 +1,790 @@
+let readerLocations = {
+  "type": "FeatureCollection",
+
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "439 Main (Eco House)"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84704988785259,
+          35.50319649069249
+        ],
+        "type": "Point"
+      },
+      "id": 0
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "WDAV"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84737194841523,
+          35.50271482792675
+        ],
+        "type": "Point"
+      },
+      "id": 1
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "VAC"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84809336959813,
+          35.50166777823718
+        ],
+        "type": "Point"
+      },
+      "id": 2
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Technology and Innovation"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84829713525212,
+          35.5004563106375
+        ],
+        "type": "Point"
+      },
+      "id": 3
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "College Store"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84841355600962,
+          35.50007895666407
+        ],
+        "type": "Point"
+      },
+      "id": 4
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "DCPC"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84753389558587,
+          35.49951366246691
+        ],
+        "type": "Point"
+      },
+      "id": 5
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Watson"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84668189901707,
+          35.49955086854884
+        ],
+        "type": "Point"
+      },
+      "id": 6
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Dana"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.8467765653026,
+          35.49924790423837
+        ],
+        "type": "Point"
+      },
+      "id": 7
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Sloane"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84589887960067,
+          35.49925998890798
+        ],
+        "type": "Point"
+      },
+      "id": 8
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wall"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84480664621175,
+          35.499008057753684
+        ],
+        "type": "Point"
+      },
+      "id": 9
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Preyer"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84649724562452,
+          35.49856061589804
+        ],
+        "type": "Point"
+      },
+      "id": 10
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Hamilton House"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84350866841741,
+          35.49767431570025
+        ],
+        "type": "Point"
+      },
+      "id": 11
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Baker Sports"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84082820241557,
+          35.49925636886334
+        ],
+        "type": "Point"
+      },
+      "id": 12
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Library"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84396048994451,
+          35.499741037044814
+        ],
+        "type": "Point"
+      },
+      "id": 13
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Chambers"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84517003838147,
+          35.499989394066304
+        ],
+        "type": "Point"
+      },
+      "id": 14
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Knobloch Campus Center"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84251603238586,
+          35.500576398303224
+        ],
+        "type": "Point"
+      },
+      "id": 15
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Duke"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.8434855259802,
+          35.50072046882697
+        ],
+        "type": "Point"
+      },
+      "id": 16
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Sentelle"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84409415076458,
+          35.500973121879724
+        ],
+        "type": "Point"
+      },
+      "id": 17
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Cannon"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84449096249261,
+          35.50109167102946
+        ],
+        "type": "Point"
+      },
+      "id": 18
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Watts"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84527002348936,
+          35.50133765985032
+        ],
+        "type": "Point"
+      },
+      "id": 19
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Little"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84561027744202,
+          35.50147410068385
+        ],
+        "type": "Point"
+      },
+      "id": 20
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Cunningham"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84694665631496,
+          35.50139868365602
+        ],
+        "type": "Point"
+      },
+      "id": 21
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Carnegie Guest House"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84613980487028,
+          35.50160303948077
+        ],
+        "type": "Point"
+      },
+      "id": 22
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Belk"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.8446686663239,
+          35.501776193265115
+        ],
+        "type": "Point"
+      },
+      "id": 23
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Residence Life"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84406582245218,
+          35.50181439718227
+        ],
+        "type": "Point"
+      },
+      "id": 24
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Lula Bell's"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.8439142089914,
+          35.501720356682526
+        ],
+        "type": "Point"
+      },
+      "id": 25
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Qdoba"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84182614673239,
+          35.50088695707646
+        ],
+        "type": "Point"
+      },
+      "id": 26
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Armfield"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84004169206948,
+          35.50163165826433
+        ],
+        "type": "Point"
+      },
+      "id": 27
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Soccer Building"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.83943130610878,
+          35.50136995507759
+        ],
+        "type": "Point"
+      },
+      "id": 28
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ryburn"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84087727405546,
+          35.50221808337453
+        ],
+        "type": "Point"
+      },
+      "id": 29
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Daley"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84048538184574,
+          35.502425025845014
+        ],
+        "type": "Point"
+      },
+      "id": 30
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Jamieson"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84005818403159,
+          35.502620471021075
+        ],
+        "type": "Point"
+      },
+      "id": 31
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Hart"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.8407325210769,
+          35.50275843203619
+        ],
+        "type": "Point"
+      },
+      "id": 32
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Flowe"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84166458539163,
+          35.502675080238745
+        ],
+        "type": "Point"
+      },
+      "id": 33
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Davis"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84147746568766,
+          35.50292513423355
+        ],
+        "type": "Point"
+      },
+      "id": 34
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Tomlinson"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84511815043311,
+          35.50304914816466
+        ],
+        "type": "Point"
+      },
+      "id": 35
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Richardson"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84475167128512,
+          35.50252251774336
+        ],
+        "type": "Point"
+      },
+      "id": 36
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Student Health"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84613098580834,
+          35.50336859404794
+        ],
+        "type": "Point"
+      },
+      "id": 37
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "The Hurt Hub"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84838151554399,
+          35.504245490830115
+        ],
+        "type": "Point"
+      },
+      "id": 38
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Chidsey"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84474072300554,
+          35.50356845211732
+        ],
+        "type": "Point"
+      },
+      "id": 39
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Akers"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84248770359494,
+          35.50452983784555
+        ],
+        "type": "Point"
+      },
+      "id": 40
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Knox"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84233976165154,
+          35.50412951756074
+        ],
+        "type": "Point"
+      },
+      "id": 41
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Irwin"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84300445855379,
+          35.50452135650373
+        ],
+        "type": "Point"
+      },
+      "id": 42
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Richardson Stadium"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84325538892911,
+          35.499690721828614
+        ],
+        "type": "Point"
+      },
+      "id": 43
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC01"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84427239282593,
+          35.503085943100814
+        ],
+        "type": "Point"
+      },
+      "id": 44
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC02"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84412639847018,
+          35.503459890274954
+        ],
+        "type": "Point"
+      },
+      "id": 45
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC03"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84387713981432,
+          35.50380194895614
+        ],
+        "type": "Point"
+      },
+      "id": 46
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC04"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84369181119212,
+          35.50407146828135
+        ],
+        "type": "Point"
+      },
+      "id": 47
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC05"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84322178058396,
+          35.503998998586624
+        ],
+        "type": "Point"
+      },
+      "id": 48
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC06"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.8428835009795,
+          35.503784487907225
+        ],
+        "type": "Point"
+      },
+      "id": 49
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC07"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84254766897136,
+          35.50359425045244
+        ],
+        "type": "Point"
+      },
+      "id": 50
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC08"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.8422307543947,
+          35.503333357695354
+        ],
+        "type": "Point"
+      },
+      "id": 51
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC10"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84252986478197,
+          35.50258835926816
+        ],
+        "type": "Point"
+      },
+      "id": 52
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC11"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84277912388252,
+          35.50232757939381
+        ],
+        "type": "Point"
+      },
+      "id": 53
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC12"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84314945102844,
+          35.502107266535816
+        ],
+        "type": "Point"
+      },
+      "id": 54
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PC13"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.84174816552004,
+          35.50357744060415
+        ],
+        "type": "Point"
+      },
+      "id": 55
+    }
+  ]
+}
